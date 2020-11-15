64813d343a91d973e09d20e654a03b4e8de031a6
Hugh Evans <hughevaans01@outlook.com>
2020-11-15 12:44:10 +0000

    Moved story to a dedicated file

--- scenes/room.js
+++ scenes/room.js
@@ -29,23 +29,11 @@ var Room = new Phaser.Class({
       this.load.image('speechBubble2', './assets/speechBubble2.png');
 
       this.load.image('mapIcon', './assets/mapIcon.png');
+
+      this.load.json('story', './story.json');
     },
     create: function() {
-      this.story = {"Monday":{"09:00":{
-                    "conferenceRoom":{character:"lincoln1",dialogue:[{message:"Welcome to Moonshot Inc. I'm Lincoln Rust the CEO\nand owner. How was your journey in?",options:[{text:"My journey was fine, thanks.",link:1},{text:"I was attacked\nby a swarm of bees.",link:1}]},
-                                                                     {message:"Awesome. Would you like me to give you a quick rundown\nof your new role here?",options:[{text:"No [SKIP TUTORIAL]",link:-1},{text:"Sure",link:2}]},
-                                                                     {message:"We need the code for our new rocket debugged before\nthe launch test on Friday: that's where you come in.",options:[{text:"Got it",link:4},{text:"Bugs?",link:3}]},
-                                                                     {message:"Our server is full of bugs and we need you to hit all\nthose bugs with a mallet. Smush em' good.",options:[{text:"I don't think\nthat's how-",link:4},{text:"Bring it on",link:4}]},
-                                                                     {message:"You'll work from the admin building. It's marked on\nyour map. Oh and make sure you only travel by map.",options:[{text:"Okay",link:6},{text:"Travel by what?",link:5}]},
-                                                                     {message:'Some of our "interns" broke lose and overan the\ngrounds. Select a location on your map to travel there.',options:[{text:"Got it",link:6},{text:"Interns?",link:7}]},
-                                                                     {message:"Feel free to explore and meet your new colleagues\njust make sure you get that engine debugged by Friday!",options:[{text:"I won't let\nyou down!",link:-1},{text:"I'm still\nconfused",link:-1}]},
-                                                                     {message:"Ah that reminds me, stay out of the containment area\nin the R&D block. I don't want to replace another dev." ,options:[{text:"Replace another-",link:6},{text:"You didn't-",link:6}]}]},
-                    "storageRoom":{character:"bort1",dialogue:[{message:"Muh?",options:[{text:"Hur",link:-1},{text:"Dur",link:-1}]}]},
-                    "labRoom":{character:"marjot1",dialogue:[{message:"Muh?",options:[{text:"Hur",link:-1},{text:"Dur",link:-1}]}]},
-                    "workshop":{character:"susan1",dialogue:[{message:"Muh?",options:[{text:"Hur",link:-1},{text:"Dur",link:-1}]}]},
-                    "containment":{character:"alien1",dialogue:[{message:"Muh?",options:[{text:"Hur",link:-1},{text:"Dur",link:-1}]}]},
-                    "observatory":{character:"astronaut1",dialogue:[{message:"Muh?",options:[{text:"Hur",link:-1},{text:"Dur",link:-1}]}]}
-                 }}};
+      this.story = this.cache.json.get('story');
 
       this.messageText;
       this.optionText1;
