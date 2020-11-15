92c1711c697a89faccf703f5f7666de46db1f45e
Hugh Evans <hughevaans01@outlook.com>
2020-11-15 12:28:52 +0000

    Added very basic debugging minigame

--- scenes/minigame.js
+++ scenes/minigame.js
@@ -5,7 +5,7 @@ var Minigame = new Phaser.Class({
     },
     init: function(data) {
       this.day = data.day;
-      this.time = data.time;
+      this.currentTime = data.time;
       this.room = data.room;
     },
     preload: function() {
@@ -14,8 +14,11 @@ var Minigame = new Phaser.Class({
       this.load.image('mugIcon', './assets/mugIcon.png');
       this.load.image('mapIcon', './assets/mapIcon.png');
       this.load.image('server', './assets/server.png');
+      this.load.image('bug1', './assets/bug1.png');
+      this.load.image('splat', './assets/splat.png');
     },
     create: function() {
+
       this.add.image(400, 300, 'metalFloor');
       this.add.image(400, 300, 'server');
       this.add.image(600,100, 'mugIcon');
@@ -24,15 +27,42 @@ var Minigame = new Phaser.Class({
       this.mallet.on('pointerover', function(){this.mallet.setTint(0xff8f00);}, this)
       this.mallet.on('pointerout', function(){this.mallet.setTint(0xffffff);}, this)
       this.mallet.on('pointerdown', function(){
-        console.log("Game start")
+        this.startGame();
+        this.mallet.visible = false;
       }, this);
 
       this.map = this.add.sprite(750, 550, 'mapIcon').setInteractive();
       this.map.on('pointerover', function(){this.map.setTint(0xff8f00);}, this)
       this.map.on('pointerout', function(){this.map.setTint(0xffffff);}, this)
       this.map.on('pointerdown', function(){
-        this.scene.start("Map",{day:this.day,time:this.time,room:this.room});
+        this.scene.start("Map",{day:this.day,time:this.currentTime,room:this.room});
       }, this);
+    },
+    // Setup game timers and win conditions
+    startGame: function() {
+      this.map.visible = false;
+      this.bugs = [];
+      this.spawnTimer = this.time.addEvent({delay: 500,  callback: this.spawnBugs, callbackScope: this,  repeat: 32});
+      this.gameTimer = this.time.delayedCall(16000, (a) => {
+        this.spawnTimer.remove();
+        this.map.visible = true;
+      });
+    },
+    // Spawns clickable bugs
+    spawnBugs: function() {
+          var x = Phaser.Math.Between(50, 750);
+          var y = Phaser.Math.Between(50, 550);
+          var bugId = this.bugs.length + 1;
+          var color = new Phaser.Display.Color(Phaser.Math.Between(0, 255),
+                                               Phaser.Math.Between(0, 255),
+                                               Phaser.Math.Between(0, 255));
+
+          this.bugs[bugId] = this.add.sprite(x, y, 'bug1').setInteractive();
+          this.bugs[bugId].on('pointerdown', function(){
+            this.spawnTimer.delay=this.spawnTimer.delay-5;
+            this.bugs[bugId].setTexture('splat').setTint(color.color);
+            this.time.delayedCall(300, (a) => {this.bugs[bugId].destroy()});
+          }, this);
     },
     update: function() {}
 });
